0f52fbbd576605c4a5c01b59244966d5ec3b5b4a
Fars <falk@infosys.com>
2025-02-22 16:42:07 -0600

    capturing location

--- badgermate/business/front_end/src/App.js
+++ badgermate/business/front_end/src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import MapSelector from "./MapSelector";
 
 const App = () => {
-  const handleLocationSelect = async (location) => {
-    console.log("Selected location:", location);
+  const [selectedAreas, setSelectedAreas] = useState([]);
 
-    await fetch("http://localhost:5000/save_location", {
+  const handleNext = () => {
+    if (selectedAreas.length === 0) {
+      alert("Please select at least one area!");
+      return;
+    }
+
+    console.log("User selected areas:", selectedAreas);
+
+    fetch("http://localhost:5000/save_selected_areas", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ lat: location.lat, lng: location.lng }),
+      body: JSON.stringify({ selectedAreas }),
     })
       .then((res) => res.json())
       .then((data) => console.log("Response from backend:", data))
@@ -17,8 +24,24 @@ const App = () => {
 
   return (
     <div>
-      <h1>Select a Location in Madison</h1>
-      <MapSelector onLocationSelect={handleLocationSelect} />
+      <h1>Select Areas in Madison</h1>
+      <MapSelector onLocationSelect={setSelectedAreas} />
+
+      <div>
+        <h3>Selected Areas:</h3>
+        <ul>
+          {selectedAreas.map((area, index) => (
+            <li key={index}>{area}</li>
+          ))}
+        </ul>
+      </div>
+
+      <button
+        onClick={handleNext}
+        style={{ marginTop: "20px", padding: "10px", fontSize: "16px" }}
+      >
+        Next
+      </button>
     </div>
   );
 };
