ca6fb7290afc0c7bdc78148faf9b463bb6e81a0e
Rashed Almansoori <ralmansoori@wisc.edu>
2025-02-22 14:15:22 -0600

    name change

---
+++ badgermate/src/pages/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{
+      backgroundColor: '#333',
+      color: '#fff',
+      padding: '20px',
+      textAlign: 'center',
+      marginBottom: '20px'
+    }}>
+      <h1>BadgerMate</h1>
+    </header>
+  );
+};
+
+export default Header;
+
