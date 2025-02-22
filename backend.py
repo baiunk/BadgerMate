df4c2ad6a5ad837e1d43812d72bb6089b3a6a73b
MARWAN ALMAZROY <almazroy@wisc.edu>
2025-02-22 12:49:39 -0600

    initial stuff

--- backend.py
+++ backend.py
@@ -1,1 +1,14 @@
-print("hello world")
+from dotenv import load_dotenv
+import os
+from flask import Flask
+from flask_cors import CORS
+from pymongo import MongoClient
+
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
+
+app = Flask(__name__)
+CORS(app)
+
+client = MongoClient(MONGO_URI)
+
